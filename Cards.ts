e0890538375f5437a82813a8fc4a241a0954c558
Trust Migisha <trust.migisha@gmail.com>
2025-03-10 13:58:14 -0600

    New Card class

---
+++ Cards.ts
@@ -0,0 +1,15 @@
+class Card {
+    private cardValue: number;
+    
+    constructor(cardValue: number) {
+        this.cardValue = cardValue;
+    }
+
+    setValue(newValue: number) {
+        this.cardValue = newValue;
+    }
+
+    getValue() {
+        return this.cardValue;
+    }
+}
