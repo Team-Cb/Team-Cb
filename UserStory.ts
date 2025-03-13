4a86c70af5fa053ef756d0f327e383851b3889c4
Faith Ewing <faithewing15@gmail.com>
2025-03-13 00:29:50 -0600

    Changed StoryQueue

--- UserStory.ts
+++ UserStory.ts
@@ -1,5 +1,44 @@
 class UserStory {
+    private name: string;
+    private description: string;
+    private storyValues: number;
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+    public setStoryValues(): void {
+        this.storyValues = 0;
+    }
 
+    public setDescription(): void { }
+
+    public getStoryValues(): number {
+        return this.storyValues;
+    }
+
+    public getDescription(): string {
+        return `${this.description}`;
+    }
+    
+    public toString(): string {
+        return `${this.name}`;
+    }
 }
 
-export {UserStory};
+class StoryQueue {
+    private stories: UserStory[];
+    constructor() {
+        this.stories = [];
+    }
+    // add to the array
+    public addStory(): void {
+        this.stories.push(this.stories[]);
+
+    }
+    // get the next element in the array
+    public nextStory(): UserStory | undefined {
+        //guard statement - if else
+        return this.stories.pop();
+    }
+}
+export {UserStory, StoryQueue};
